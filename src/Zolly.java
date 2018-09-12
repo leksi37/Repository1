e94dd70d65ba0b9b0f4fe2a3f1d5253dab847fa4
Aleksandra <aalexandrova11437@gmail.com>
2018-09-12 15:54:30 +0200

    initial commit

---
+++ src/Zolly.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Zolly {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		System.out.println("Hey, what is your name? ");
+		String name = input.nextLine();
+		System.out.println("Hey, " + name + " good luck tomorrow! ^^");
+		
+	}
+}
